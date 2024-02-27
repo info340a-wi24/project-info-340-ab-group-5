6c4afebf7f647e150fcfd57efcab3aa2f3c14415
Noah McMahon <nam1414@uw.edu>
2024-02-26 18:55:32 -0800

    Added Some State Variables

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'; //import React Component
+
+function App(props)
+{
+    const [calories, setCalories] = useState(0);
+    const [workoutTime, setWorkoutTime] = useState(0);
+}
+
+export default App;
