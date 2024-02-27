2a18165b57994688519d76035c3c7f8944f4f407
Noah McMahon <nam1414@uw.edu>
2024-02-26 19:33:20 -0800

    Added Base JS Files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'; //import React Component
 
+// Importing Other Components Here
+import Home from './Home';
+import Food from './Food';
+import Profile from './Profile';
+import SignIn from './SignIn';
+import Workout from './Workout';
+
 function App(props)
 {
     const [calories, setCalories] = useState(0);
