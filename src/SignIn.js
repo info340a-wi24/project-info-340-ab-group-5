3a47a4fd5e10e9f280222872422798ed70fa8ae6
Noah McMahon <nam1414@uw.edu>
2024-02-28 15:55:49 -0800

    Started Sign-In Page

--- src/SignIn.js
+++ src/SignIn.js
@@ -3,6 +3,30 @@ import React, { useState } from 'react'; //import React Component
 export default function SignIn(props)
 {
     return (
-        <p>SignIn Page Here</p>
+        <div>
+            <main>
+                <section>
+                    <h2>Please Input Your Information</h2>
+                    <div className='form-group'>
+                        <label>Username:</label>
+                    </div>
+                    <div className='form-group'>
+                        <label>Gender:</label>
+                    </div>
+                    <div className='form-group'>
+                        <label>Height:</label>
+                    </div>
+                    <div className='form-group'>
+                        <label>Weight:</label>
+                    </div>
+                    <div className='form-group'>
+                        <label>Weight Goal:</label>
+                    </div>
+                    <div className='form-group'>
+                        <label>Daily Calorie Goal:</label>
+                    </div>
+                </section>
+            </main>
+        </div>
     );
 }
